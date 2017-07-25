c7a13f8a3ba3f5997a39fe404c234b816b52b954
Sebastian <cebasfu93@gmail.com>
2017-07-24 19:01:53 -0500

    clase Atomo

---
+++ atom.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Atom:
+    def __init__(self, x , y, z, q, name, tipo):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.q = q
+        self.name = name
+        self.type = tipo 
