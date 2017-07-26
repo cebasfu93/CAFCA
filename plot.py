182bb24dad440d2798f06e5825af689a2b47f369
Sebastian <cebasfu93@gmail.com>
2017-07-25 20:12:47 -0500

    Plot function generalized

--- plot.py
+++ plot.py
@@ -2,17 +2,24 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+def scatter3d(xs, ys, zs, color, plot_name, xlabel='X', ylabel='Y', zlabel='Z'):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    colors=ax.scatter(xs, ys, zs, c=color, cmap='seismic')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+    ax.set_aspect('equal')
+    #ax.set_aspect('equal', 'datalim')
+    fig.colorbar(colors)
+    plt.savefig(plot_name+'.pdf', format='pdf')
+    #plt.show()
+    plt.close()
+
 #importa info de los atomos
 atoms=np.genfromtxt('atomos.outc', skip_header=1)
+norm_vel=np.linalg.norm(atoms[:,3:6], axis=1)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-colors=ax.scatter(atoms[:,0], atoms[:,1], atoms[:,2], c=atoms[:,3], cmap='seismic')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.set_aspect('equal')
-#ax.set_aspect('equal', 'datalim')
-fig.colorbar(colors)
-plt.savefig('Initial.pdf', format='pdf')
-plt.show()
+scatter3d(atoms[:,0], atoms[:,1], atoms[:,2], atoms[:,6], 'Initial_realq')
+scatter3d(atoms[:,3], atoms[:,4], atoms[:,5], atoms[:,6], 'Initial_velsq', 'VX', 'VY', 'VZ')
+scatter3d(atoms[:,0], atoms[:,1], atoms[:,2], norm_vel, 'Initial_realv')
