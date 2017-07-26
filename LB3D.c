92cfef19fb975f83289c29759314cd3496db2226
Sebastian <cebasfu93@gmail.com>
2017-07-26 18:09:15 -0500

    imports names and types. Cant print

--- LB3D.c
+++ LB3D.c
@@ -13,7 +13,7 @@ FLOAT Vx_min, Vx_max, Vy_min, Vy_max, Vz_min, Vz_max, Vx, Vy, Vz;
 FLOAT delx, dely, delz, delvx, delvy, delvz;
 
 FILE *const_file, *coor_file, *speeds_file, *names_file, *types_file, *charges_file;
-FLOAT *coorx, *coory, *coorz, *velx, *vely, *velz, *names, *types, *charges;
+FLOAT x_ini, y_ini, z_ini, vx_ini, vy_ini, vz_ini, q_ini;
 FLOAT *x_space, *v_space;
 
 //-------------------------Main-------------------------//
@@ -25,56 +25,53 @@ int main(int argc, char const *argv[]){
 
   coor_file = fopen("coords.outpy", "r");
   speeds_file = fopen("speeds.outpy", "r");
-  names_file = fopen("names.outpy", "r");
-  types_file = fopen("types.outpy", "r");
   charges_file = fopen("charges.outpy", "r");
   N_atoms=countlines(coor_file);
   fclose(coor_file);
   coor_file = fopen("coords.outpy", "r");
-
-  coorx=malloc(N_atoms*sizeof(FLOAT));
-  coory=malloc(N_atoms*sizeof(FLOAT));
-  coorz=malloc(N_atoms*sizeof(FLOAT));
-  velx=malloc(N_atoms*sizeof(FLOAT));
-  vely=malloc(N_atoms*sizeof(FLOAT));
-  velz=malloc(N_atoms*sizeof(FLOAT));
-  charges=malloc(N_atoms*sizeof(FLOAT));
-  names=malloc(N_atoms*sizeof(FLOAT));
-  types=malloc(N_atoms*sizeof(FLOAT));
-  check(coorx); check(coory); check(coorz); check(charges);
-
-  for(i=0;i<N_atoms;i++){
-    useless=fscanf(coor_file, "%lf %lf %lf", &coorx[i], &coory[i], &coorz[i]);
-    useless=fscanf(speeds_file, "%lf %lf %lf", &velx[i], &vely[i], &velz[i]);
-    useless=fscanf(charges_file, "%lf", &charges[i]);
-  }
-
-  fclose(coor_file);
-  fclose(names_file);
-  fclose(types_file);
-  fclose(charges_file);
-
-  print_atoms(coorx, coory, coorz, velx, vely, velz, charges, names, types);
-
 
   x_space=malloc(N_res*N_res*N_res*sizeof(FLOAT));
   v_space=malloc(N_res*N_res*N_res*sizeof(FLOAT));
   check(x_space); check(v_space);
 
   for(i=0;i<N_atoms;i++){
-    x_space[ndx(coorx[i], coory[i], coorz[i], 'x')]=charges[i];
-    v_space[ndx(velx[i], vely[i], velz[i], 'v')]=charges[i];
+    useless=fscanf(coor_file, "%lf %lf %lf", &x_ini, &y_ini, &z_ini);
+    useless=fscanf(speeds_file, "%lf %lf %lf", &vx_ini, &vy_ini, &vz_ini);
+    useless=fscanf(charges_file, "%lf", &q_ini);
+    x_space[ndx(x_ini, y_ini, z_ini, 'x')]=q_ini;
+    v_space[ndx(vx_ini, vy_ini, vz_ini, 'v')]=q_ini;
   }
+
+  fclose(coor_file);
+  fclose(names_file);
+  fclose(charges_file);
+
+  names_file=fopen("names.outpy", "r");
+  types_file = fopen("types.outpy", "r");
+
+  const char *names[N_atoms], *types[N_atoms];
+  char name_temp[5], type_temp[5];
+  for(i=0;i<N_atoms;i++){
+    useless=fscanf(names_file, "%s", name_temp);
+    useless=fscanf(types_file, "%s", type_temp);
+    names[i]=name_temp;
+    types[i]=type_temp;
+  }
+  fclose(names_file);
+  fclose(types_file);
+
+  //print_atoms(coorx, coory, coorz, velx, vely, velz, charges, names, types);
+
   return 0;
 }
 
 //-------------------------Funciones-------------------------//
-void print_atoms(FLOAT *atom_x, FLOAT *atom_y, FLOAT *atom_z, FLOAT *atom_vx, FLOAT *atom_vy, FLOAT *atom_vz, FLOAT *atom_charges, FLOAT *atom_names, FLOAT *atom_types){
+void print_atoms(FLOAT *atom_x, FLOAT *atom_y, FLOAT *atom_z, FLOAT *atom_vx, FLOAT *atom_vy, FLOAT *atom_vz, FLOAT *atom_charges, char **atom_names, char **atom_types){
   FILE *atoms_file;
   atoms_file = fopen("atomos.outc", "w");
   fprintf(atoms_file, "Coordenadas (x,y,z) \t Velocidades (vx, vy, vz) \t carga \t nombre \t tipo \n");
   for(i=0;i<N_atoms;i++){
-    fprintf(atoms_file, "%lf %lf %lf %lf %lf %lf %lf %lf %lf \n", atom_x[i], atom_y[i], atom_z[i], atom_vx[i], atom_vy[i], atom_vz[i], atom_charges[i], atom_names[i], atom_types[i]);
+    fprintf(atoms_file, "%lf %lf %lf %lf %lf %lf %lf %s %s \n", atom_x[i], atom_y[i], atom_z[i], atom_vx[i], atom_vy[i], atom_vz[i], atom_charges[i], atom_names[i], atom_types[i]);
   }
   fclose(atoms_file);
 }
