873b0b5d6a5e07a90e7104978da5eda8f31f38d8
Sebastian <cebasfu93@gmail.com>
2017-07-29 14:49:11 -0500

    Includes dictionary of atomic numbers and elements

--- atom.py
+++ atom.py
@@ -11,3 +11,14 @@ class Atom:
         self.q = q
         self.name = name
         self.type = tipo
+
+atomic_numbers={}
+
+el_names=['H', 'He',
+'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
+'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr']
+for i in range(len(el_names)):
+    atomic_numbers[el_names[i]]=i+1
+
+print atomic_numbers
