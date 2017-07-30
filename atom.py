1c493b207570b8c430f688c9f9bcb5e788a79c9a
Sebastian <cebasfu93@gmail.com>
2017-07-30 18:16:45 -0500

    new attribute with string of the other attr.

--- atom.py
+++ atom.py
@@ -11,14 +11,15 @@ class Atom:
         self.q = q
         self.name = name
         self.type = tipo
+        self.info = map(str,[self.name, self.type, self.x, self.y, self.z, self.vx, self.vy, self.vz, self.q])
 
-atomic_numbers={}
-
+#Define lista con nombres de elementos admitidos
 el_names=['H', 'He',
 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar',
 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr']
+
+#Define diccionario que le da numero atomico a cada elemento
+atomic_numbers={}
 for i in range(len(el_names)):
     atomic_numbers[el_names[i]]=i+1
-
-print atomic_numbers
