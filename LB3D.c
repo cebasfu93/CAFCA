f9ae6b756df69b5bc7caeae8a8d37e2aa87dee30
Sebastian <cebasfu93@gmail.com>
2017-08-07 18:25:22 -0500

    arrays type int are now unsigned int for memory optimization

--- LB3D.c
+++ LB3D.c
@@ -6,16 +6,14 @@
 #include "funciones.c"
 
 //-------------------------Variables globales-------------------------//
-int i,j,k, N_atoms, Nx, Ny, Nz, Nvx, Nvy, Nvz, useless, Nxtot, Nvtot, rvdw;
-int x_ini, y_ini, z_ini, vx_ini, vy_ini, vz_ini, N_ini;
+int i,j,k, N_atoms, Nx, Ny, Nz, Nvx, Nvy, Nvz, useless, Nxtot, Nvtot;
 
 int Lx, Ly, Lz;
 int Vx, Vy, Vz;
-FLOAT dV, q_ini, qpc_ini;
 
-FILE *atoms_file, *atoms_testp;
-FLOAT *qpercube;
-int *N_elec;
+FILE *atoms_file;
+FLOAT *q_nuc, *qpc_nuc;
+unsigned int *x_nuc, *y_nuc, *z_nuc, *vx_nuc, *vy_nuc, *vz_nuc, *rvdw_nuc, *N_elec;
 
 char name_temp[5], type_temp[5], el_temp[3];
 
@@ -23,9 +21,11 @@ char name_temp[5], type_temp[5], el_temp[3];
 int main(int argc, char const *argv[]){
 
   assign_cons();
-  calc_qpercube();
+  init_molecule();
 
-  //init_molecule();
+  printf("%s %s %s %u %u %u %u %u %u %.8lf %u %u %.8lf \n", elements[i], names[i], types[i], x_nuc[i], y_nuc[i], z_nuc[i], vx_nuc[i], vy_nuc[i], vz_nuc[i],
+  q_nuc[i], rvdw_nuc[i], N_elec[i], qpc_nuc[i]);
+
   //atoms_file = fopen("atomos.outpy", "r");
 
   /*const char *names[N_atoms], *types[N_atoms], *elements[N_atoms];
@@ -51,31 +51,34 @@ int main(int argc, char const *argv[]){
 }
 
 //-------------------------Funciones-------------------------//
-void calc_qpercube(){
+
+void init_molecule(){
+  static const char *names[N_atoms], *types[N_atoms], *elements[N_atoms];
   atoms_file = fopen("atomos.outpy", "r");
+
+  x_nuc=malloc(sizeof(int)*N_atoms); checkuint(x_nuc); inituint(x_nuc, N_atoms);
+  y_nuc=malloc(sizeof(int)*N_atoms); checkuint(y_nuc); inituint(y_nuc, N_atoms);
+  z_nuc=malloc(sizeof(int)*N_atoms); checkuint(z_nuc); inituint(z_nuc, N_atoms);
+  vx_nuc=malloc(sizeof(int)*N_atoms); checkuint(vx_nuc); inituint(vx_nuc, N_atoms);
+  vy_nuc=malloc(sizeof(int)*N_atoms); checkuint(vy_nuc); inituint(vy_nuc, N_atoms);
+  vz_nuc=malloc(sizeof(int)*N_atoms); checkuint(vz_nuc); inituint(vz_nuc, N_atoms);
+  q_nuc=malloc(sizeof(FLOAT)*N_atoms); checkfloat(q_nuc); initfloat(q_nuc, N_atoms);
+  rvdw_nuc=malloc(sizeof(int)*N_atoms); checkuint(rvdw_nuc); inituint(rvdw_nuc, N_atoms);
+  N_elec=malloc(sizeof(int)*N_atoms); checkuint(N_elec); inituint(N_elec, N_atoms);
+  qpc_nuc=malloc(sizeof(FLOAT)*N_atoms); checkfloat(qpc_nuc); initfloat(qpc_nuc, N_atoms);
+
   for(i=0;i<N_atoms;i++){
-    useless=fscanf(atoms_file, "%s %s %s %d %d %d %d %d %d %lf %d %d %lf", el_temp, name_temp, type_temp, &x_ini, &y_ini, &z_ini, &vx_ini, &vy_ini, &vz_ini, &q_ini, &rvdw, &N_ini, &qpc_ini);
-    N_elec[i]=N_ini;
-    qpercube[i]=qpc_ini;
-    printf("%lf, %d \n", qpercube[i], N_elec[i]);
+    useless=fscanf(atoms_file, "%s %s %s %u %u %u %u %u %u %lf %u %u %lf", el_temp, name_temp, type_temp, &x_nuc[i], &y_nuc[i], &z_nuc[i], &vx_nuc[i], &vy_nuc[i], &vz_nuc[i],
+    &q_nuc[i], &rvdw_nuc[i], &N_elec[i], &qpc_nuc[i]);
+    elements[i]=el_temp;
+    names[i]=name_temp;
+    types[i]=type_temp;
+    printf("%s %s %s %u %u %u %u %u %u %.8lf %u %u %.8lf \n", elements[i], names[i], types[i], x_nuc[i], y_nuc[i], z_nuc[i], vx_nuc[i], vy_nuc[i], vz_nuc[i],
+    q_nuc[i], rvdw_nuc[i], N_elec[i], qpc_nuc[i]);
   }
   fclose(atoms_file);
 }
 
-void init_molecule(){
-  /*xs=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(xs); ys=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(ys); zs=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(zs);
-  vxs=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(vxs); vys=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(vys); vzs=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(vzs);
-  qs=malloc(sizeof(FLOAT)*(Nbod+N_atoms)); checkfloat(xs);
-
-  atoms_file = fopen("atomos.outpy", "r");
-  for(i=0;i<N_atoms;i++){
-    useless=fscanf(atoms_file, "%s %s %s %lf %lf %lf %lf %lf %lf %lf %lf", el_temp, name_temp, type_temp, &x_ini, &y_ini, &z_ini, &vx_ini, &vy_ini, &vz_ini, &q_ini, &rvdw);
-    xs[i]=(int) x_ini/delx; ys[i]=(int) y_ini/delx; zs[i]=(int) z_ini/delx;
-    vxs[i]=(int) vx_ini/delv; vys[i]=(int) vy_ini/delv; vzs[i]=(int) vz_ini/delv;
-    qs[i]= q_ini;
-  }
-  fclose(atoms_file);*/
-}
 void print_atoms(FLOAT *atom_x, FLOAT *atom_y, FLOAT *atom_z, FLOAT *atom_vx, FLOAT *atom_vy, FLOAT *atom_vz, FLOAT *atom_charges, char **atom_names, char **atom_types){
   FILE *atoms_file;
   atoms_file = fopen("atomos.outc", "w");
@@ -100,14 +103,11 @@ void assign_cons(){
   fclose(cons_file);
 
   Nxtot=Lx*Ly*Lz; Nvtot=Vx*Vy*Vz;
-
-  N_elec=malloc(sizeof(int)*N_atoms); initint(N_elec, N_atoms);
-  qpercube=malloc(sizeof(FLOAT)*N_atoms); initfloat(qpercube, N_atoms);
 }
 int coor2ndx(FLOAT coor1, FLOAT coor2, FLOAT coor3, char state){
   int indx, indy, indz;
   int res;
-  if(state=='x'){
+  /*if(state=='x'){
     indx=(int) ((coor1-Lx_min)/delx);
     indy=(int) ((coor2-Ly_min)/delx);
     indz=(int) ((coor3-Lz_min)/delx);
@@ -118,16 +118,16 @@ int coor2ndx(FLOAT coor1, FLOAT coor2, FLOAT coor3, char state){
     indy=(int) ((coor2-Vy_min)/delv);
     indz=(int) ((coor3-Vz_min)/delv);
     res=ndx(indx, indy, indz, 'v');
-  }
+  }*/
   return res;
 }
 int ndx(int indi, int indj, int indk, char state){
   int res;
-  if(state=='x'){
+  /*if(state=='x'){
     res=indi + Nx*(indj+Ny*indk);
   }
   else if(state=='v'){
     res=indi + Nvx*(indj+Nvy*indk);
-  }
+  }*/
   return res;
 }
