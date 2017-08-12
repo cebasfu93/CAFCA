961ec213bf7cbf0f30332f757c1efbc37d47840a
Sebastian <cebasfu93@gmail.com>
2017-08-12 17:45:06 -0500

    plots the charge of the system

---
+++ test_plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+cons=np.genfromtxt('constants.outpy')
+inp=np.genfromtxt('check.outc')
+Lx=cons[0]
+Ly=cons[1]
+Lz=cons[2]
+qs=inp[inp!=0.0]
+ind=np.where(inp!=0.0)[0]
+
+def ndx2xyz(ndx):
+    x = (ndx % (Lx*Ly)) % Lx
+    y = (ndx % (Lx*Ly)) // Lx
+    z = ndx // (Lx*Ly)
+    return x, y, z
+
+xs, ys, zs = ndx2xyz(ind)
+
+print max(qs[qs<0.8]), min(qs)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors=ax.scatter(xs, ys, zs, c=qs, cmap='seismic', vmin=-0.0007, vmax=0)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.set_aspect('equal')
+fig.colorbar(colors)
+plt.show()
+#plt.close()
