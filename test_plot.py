1b0c2581de438c5362b34d6d8b8a2d99e79dea3c
Sebastian <cebasfu93@gmail.com>
2017-08-13 16:56:28 -0500

    Plots frontiers of electronic distribution

--- test_plot.py
+++ test_plot.py
@@ -7,8 +7,11 @@ inp=np.genfromtxt('check.outc')
 Lx=cons[0]
 Ly=cons[1]
 Lz=cons[2]
-qs=inp[inp!=0.0]
-ind=np.where(inp!=0.0)[0]
+
+nx=inp[:,0]
+ny=inp[:,1]
+nz=inp[:,2]
+qs=inp[:,3]
 
 def ndx2xyz(ndx):
     x = (ndx % (Lx*Ly)) % Lx
@@ -16,11 +19,11 @@ def ndx2xyz(ndx):
     z = ndx // (Lx*Ly)
     return x, y, z
 
-xs, ys, zs = ndx2xyz(ind)
+#xs, ys, zs = ndx2xyz(ind)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-colors=ax.scatter(xs, ys, zs, c=qs, cmap='seismic', vmin=min(qs), vmax=max(qs[qs<0.8]))
+colors=ax.scatter(nx, ny, nz, c=qs, cmap='seismic', vmin=min(qs), vmax=max(qs[qs<0.8]))
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
