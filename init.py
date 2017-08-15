5f717d1c5d85d4160c76fc63d70c786df1008283
Sebastian <cebasfu93@gmail.com>
2017-08-15 17:48:52 -0500

    changed units to nm

--- init.py
+++ init.py
@@ -6,8 +6,8 @@ parser=OptionParser()
 parser.add_option("-i","--input", action="store", type="string", dest="InputFile", default='HCl.mol2', help="Path of the .mol2 input file.")
 parser.add_option("-L","--realdistance", action="store", type="float", dest="Distance", default=5.0, help="Real-space distance from atoms to simulation box (A).")
 parser.add_option("-V","--veldistance", action="store", type="float", dest="Distancev", default=5.0, help="Velocity distance from atoms to simulation box (A/ps).")
-parser.add_option("-l","--dx", action="store", type="float", dest="Dx", default=0.1, help="Resolution of the real space (A)")
-parser.add_option("-v","--dv", action="store", type="float", dest="Dv", default=0.1, help="Resolution of the velocity space (A/ps)")
+parser.add_option("-l","--dx", action="store", type="float", dest="Dx", default=0.01, help="Resolution of the real space (nm)")
+parser.add_option("-v","--dv", action="store", type="float", dest="Dv", default=0.01, help="Resolution of the velocity space (nm/ps)")
 
 
 (options, args) = parser.parse_args()
