785ee6b2a52fa4ad03b9ea17e315a030ee897592
Sebastian <cebasfu93@gmail.com>
2017-08-15 22:04:59 -0500

    fftw 3d test working

--- fftw_test/fftw_test.c
+++ fftw_test/fftw_test.c
@@ -7,27 +7,37 @@
 #define FLOAT double
 #define pi 3.1415
 
-int i,j;
+int i,j,k;
 FILE *test_result;
+FLOAT kx, ky, kz, Kx, Ky, Kz;
+
 int Nx=100;
 FLOAT Lx=1.0;
 int Ny=150;
 FLOAT Ly=2.5;
+int Nz=100;
+FLOAT Lz=1.5;
+FLOAT dx=0.01;
+
 FLOAT *arreglo;
 
 fftw_complex *rho_out, *rho_in, *rho_fin;
 fftw_plan rho_plan;
 int Ntot;
 
-int ndx(int indx, int indy);
+int ndx(int indi, int indj, int indk);
+FLOAT sinc(FLOAT x);
+void print_arr(FLOAT *array, char dir);
+void print_all(FLOAT *array);
 
 int main(){
-  Ntot=Nx*Ny;
+  Ntot=Nx*Ny*Nz;
   FLOAT dx=Lx/Nx;
   FLOAT dy=Ly/Ny;
+  FLOAT dz=Lz/Nz;
 
-  arreglo=malloc(Nx*Ny*sizeof(FLOAT));
-  test_result=fopen("fftw_test.txt", "w");
+  arreglo=malloc(Ntot*sizeof(FLOAT));
+  /*test_result=fopen("fftw_test.txt", "w");
   for(i=0;i<Nx;i++){
     for(j=0;j<Ny;j++){
       arreglo[ndx(i,j)]=sin(i*pi*dx/Lx)*sin(3*j*pi*dy/Ly);
@@ -36,6 +46,10 @@ int main(){
     fprintf(test_result, "\n");
   }
   fclose(test_result);
+  */
+  arreglo[ndx(50,75,50)]=17.0;
+  arreglo[ndx(30,75,50)]=15.0;
+  arreglo[ndx(50,20,50)]=20.0;
 
 //--------------------------------------------------------------------------------------------------------------
 
@@ -46,19 +60,26 @@ int main(){
   for(i=0;i<Ntot;i++){
     rho_in[i]=arreglo[i];
   }
-  rho_plan = fftw_plan_dft_2d(Nx, Ny, rho_in, rho_out, 1, FFTW_ESTIMATE);
+  rho_plan = fftw_plan_dft_3d(Nx, Ny, Nz, rho_in, rho_out, 1, FFTW_ESTIMATE);
   fftw_execute(rho_plan);
   fftw_destroy_plan(rho_plan);
 
-  /*for(i=0;i<Nx;i++){
+  for(i=0;i<Nx;i++){
+    kx=2*pi/Lx*((FLOAT)i+0.5);
+    Kx=kx*sinc(0.5*kx*dx);
     for(j=0;j<Ny;j++){
-      if(i==0 || i==(Nx-1) || j==0 || j==(Ny-1)){
-          rho_out[ndx(i,j)]=0.0;
+      ky=2*pi/Ly*((FLOAT)j+0.5);
+      Ky=ky*sinc(0.5*ky*dx);
+      for(k=0;k<Nz;k++){
+        kz=2*pi/Lz*((FLOAT)k+0.5);
+        Kz=kz*sinc(0.5*kz*dx);
+        rho_out[ndx(i,j,k)]=-rho_out[ndx(i,j,k)]/(pow(Kx,2)+pow(Ky,2)+pow(Kz,2));
+        //printf("%d %d %d %f %f \n", i, j, k, creal(rho_out[ndx(i,j,k)]),  creal(rho_out[ndx(i,j,k)]));
       }
     }
-  }*/
+  }
 
-  rho_plan = fftw_plan_dft_2d(Nx, Ny, rho_out, rho_fin, -1, FFTW_ESTIMATE);
+  rho_plan = fftw_plan_dft_3d(Nx, Ny, Nz, rho_out, rho_fin, -1, FFTW_ESTIMATE);
   fftw_execute(rho_plan);
   fftw_destroy_plan(rho_plan);
 
@@ -66,20 +87,79 @@ int main(){
     arreglo[i]=rho_fin[i]/Ntot;
   }
 
+  print_all(arreglo);
 //--------------------------------------------------------------------------------------------------------------
 
-test_result=fopen("fftw_test_out.txt", "w");
-for(i=0;i<Nx;i++){
-  for(j=0;j<Ny;j++){
-    fprintf(test_result, "%lf ", arreglo[ndx(i,j)]);
+
+    return 0;
+}
+
+FLOAT sinc(FLOAT x){
+  if (x==0){
+    return 1.0;
   }
-  fprintf(test_result, "\n");
+  return sin(x)/x;
 }
-fclose(test_result);
-
-  return 0;
+int ndx(int indi, int indj, int indk){
+  return indi + Nx*(indj+Ny*indk);
 }
+void print_arr(FLOAT *array, char dir){
+  int i,j,k;
+  FLOAT sum;
+  if(dir=='x'){
+    FILE *arrx_file;
+    arrx_file=fopen("arrx.outc", "a");
 
-int ndx(int indx, int indy){
-  return indy*Nx+indx;
+    for(i=0;i<Nx;i++){
+      sum=0;
+      for(j=0;j<Ny;j++){
+        for(k=0;k<Nz;k++){
+          sum+=array[ndx(i,j,k)];
+        }
+      }
+      fprintf(arrx_file, "%f \n", sum);
+    }
+    fclose(arrx_file);
+  }
+
+  else if(dir=='y'){
+    FILE *arry_file;
+    arry_file=fopen("arry.outc", "a");
+
+    for(i=0;i<Ny;i++){
+      sum=0;
+      for(j=0;j<Nz;j++){
+        for(k=0;k<Nx;k++){
+          sum+=array[ndx(k,i,j)];
+        }
+      }
+      fprintf(arry_file, "%f \n", sum);
+    }
+    fclose(arry_file);
+  }
+
+  else if(dir=='z'){
+    FILE *arrz_file;
+    arrz_file=fopen("arrz.outc", "a");
+
+    for(i=0;i<Nz;i++){
+      sum=0;
+      for(j=0;j<Nx;j++){
+        for(k=0;k<Ny;k++){
+          sum+=array[ndx(j,k,i)];
+        }
+      }
+      fprintf(arrz_file, "%f \n", sum);
+    }
+    fclose(arrz_file);
+  }
+  else{
+    printf("La direccion de la densidad que se quiere imprimir, no es valida \n");
+    exit(0);
+  }
+}
+void print_all(FLOAT *array){
+  print_arr(array, 'x');
+  print_arr(array, 'y');
+  print_arr(array, 'z');
 }
--- fftw_test/fftw_test.py
+++ fftw_test/fftw_test.py
@@ -1,8 +1,13 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import gridspec
 
-data=np.transpose(np.genfromtxt('fftw_test.txt'))
+Nx=100
+Ny=150
+Nz=100
+
+"""data=np.transpose(np.genfromtxt('fftw_test.txt'))
 data_out=np.transpose(np.genfromtxt('fftw_test_out.txt'))
 print np.shape(data)
 print np.shape(data_out)
@@ -19,3 +24,31 @@ ax.plot_wireframe(gx, gy, data_out, color='r', rstride=10, cstride=10, label='ou
 ax.plot_wireframe(gx, gy, data, rstride=10, cstride=10, label='init')
 plt.legend()
 plt.show()
+"""
+
+arrx=np.genfromtxt('arrx.outc')
+arry=np.genfromtxt('arry.outc')
+arrz=np.genfromtxt('arrz.outc')
+x=np.linspace(0,Nx-1, Nx)
+y=np.linspace(0,Ny-1, Ny)
+z=np.linspace(0,Nz-1, Nz)
+
+fig=plt.figure()
+gs=gridspec.GridSpec(1,3)
+ax0=fig.add_subplot(gs[0,0])
+ax0.plot(x, arrx)
+plt.ylabel(r'arrx')
+plt.xlabel('x')
+
+ax1=fig.add_subplot(gs[0,1])
+ax1.plot(y, arry)
+plt.ylabel(r'arry')
+plt.xlabel('y')
+
+ax2=fig.add_subplot(gs[0,2])
+ax2.plot(z, arrz)
+plt.ylabel(r'arrz')
+plt.xlabel('z')
+
+plt.savefig('arr.pdf', format='pdf')
+plt.close()
